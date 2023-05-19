95b45a0f3e2c1178fce4419a94cdfcea95ee773b
Rahul Shah <rahul107.infinity@gmail.com>
2023-05-19 12:01:59 +0700

    wip: Test with some basic background classes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-l">
       <h1 className="text-xl font-bold underline">
         Hello world!
       </h1>
